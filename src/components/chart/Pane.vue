906270b5d1f6d9e8c68303e84fc3e6e0fd6b5343
wuqian@cont.com.cn <wuqian@cont.com.cn>
2020-08-10 18:00:35 +0800

    补充

---
+++ src/components/chart/Pane.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="chart-pane">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'chart-pane'
+}
+</script>
+<style  scoped>
+#chart-pane{
+  position: absolute;
+  top: 50px;
+  right: 20px;
+  height: 800px;
+  width: 300px;
+  z-index: 1000;
+  background-color: #ffffff;
+  border-radius: 10px;
+}
+</style>
